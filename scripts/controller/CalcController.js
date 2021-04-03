9eaf2fe487b7029606b73c53bf6a6faab9694767
theusccruz <matheus8476@outlook.com>
2021-04-03 20:56:44 -0300

    feat(CalcController): add estrutura condicional para cada botão - Foi criado um switch case para decidir o que ocorre para cada botão acionado - Métodos para apagar tudo, apagar última entrada, emitir error e adicionar valor ao array de operações também foram criados

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,8 +6,9 @@ class CalcController {
         this._$date = document.querySelector('#data');
         this._$time = document.querySelector('#hora');
         this._locale = 'pt-BR';
+        this._operation = [];
         this._currentDate;
-        this.initialize();        
+        this.initialize();
     }
 
     initialize() {
@@ -20,10 +21,74 @@ class CalcController {
         this.initButtonEvents();
     }
 
-    addEventListenerAll(element, events, fn) {//trabalha com mais de um evento do DOM
+    addEventListenerAll(element, events, fn) { //trabalha com mais de um evento do DOM
         events.split(' ').forEach(event => {
             element.addEventListener(event, fn, false);
-        }); 
+        });
+    }
+
+    clearAll() {
+        this._operation = [];
+    }
+
+    clearEntry() {
+        this._operation.pop();
+        console.log(this._operation);
+    }
+
+    addOperation(value) {
+        this._operation.push(value);
+        console.log(this._operation);
+    }
+
+    setError() {
+        this.displayCalc = 'ERROR';
+    }
+
+    execBtn(value) {
+        switch (value) {
+            case 'ac':
+                this.clearAll();
+                break;
+            case 'ce':
+                this.clearEntry();
+                break;
+            case 'soma':
+
+                break;
+            case 'subtracao':
+
+                break;
+            case 'divisao':
+
+                break;
+            case 'multiplicacao':
+
+                break;
+            case 'porcento':
+
+                break;
+            case 'igual':
+
+                break;
+
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':
+                this.addOperation(parseInt(value));
+                break;
+
+            default:
+                this.setError();
+                break;
+        }
     }
 
     initButtonEvents() {
@@ -31,13 +96,14 @@ class CalcController {
 
         buttons.forEach(btn => {
             this.addEventListenerAll(btn, 'click drag', e => {
-                console.log(btn.className.baseVal.replace("btn-", ""));
+                let textBtn = btn.className.baseVal.replace("btn-", "");
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
                 btn.style.cursor = "pointer";
-            })            
-        });        
+            })
+        });
     }
 
     setDisplayDateTime() {
