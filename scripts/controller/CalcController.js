40397b8913671efaa0be7edaba44711796c6041f
theusccruz <matheus8476@outlook.com>
2021-04-03 19:59:30 -0300

    feat(CalcController): tratamendo de eventos do mouse - Os eventos click, drag, mouseup, mousedown e mouseover serão tratados com o método addEventListenerAll Made-by: Matheus Cruz Refs #1

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -6,23 +6,45 @@ class CalcController {
         this._$date = document.querySelector('#data');
         this._$time = document.querySelector('#hora');
         this._locale = 'pt-BR';
-        this._currentDate = '0';
-        this.initialize();
+        this._currentDate;
+        this.initialize();        
     }
 
     initialize() {
-        this.setDisplayDateTme();
+        this.setDisplayDateTime();
 
         setInterval(() => {
-            this.setDisplayDateTme();
+            this.setDisplayDateTime();
         }, 1000);
+
+        this.initButtonEvents();
     }
 
-    setDisplayDateTme() {
+    addEventListenerAll(element, events, fn) {//trabalha com mais de um evento do DOM
+        events.split(' ').forEach(event => {
+            element.addEventListener(event, fn, false);
+        }); 
+    }
+
+    initButtonEvents() {
+        let buttons = document.querySelectorAll('#buttons > g, #parts > g');
+
+        buttons.forEach(btn => {
+            this.addEventListenerAll(btn, 'click drag', e => {
+                console.log(btn.className.baseVal.replace("btn-", ""));
+            });
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e => {
+                btn.style.cursor = "pointer";
+            })            
+        });        
+    }
+
+    setDisplayDateTime() {
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
         this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
             day: '2-digit',
-            month: 'long',
+            month: 'short',
             year: 'numeric'
         });
     }
